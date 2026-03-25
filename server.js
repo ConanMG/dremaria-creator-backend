da87a516a52090111afc916d9dc12618b9363bbe
ConanMG <conanmontes02@gmail.com>
2026-03-25 16:43:29 +0100

    Update server.js
    + Species
       + Delete species
       + Delete race
    + Classes
       + Post class
       + Post subclass
       + Delete class
       + Delete subclass
    + Backgrounds
       + Post Background
       + Delete Background
    + Spells
       + Get by element
       + Post spell
       + Delete spell
    + Feats
       + Post feat
       + Delete feat

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.post('/species', (req, res) => {
             connection.db("mechanics").collection("species").insertOne(req.body)
                 .then(response => {
                     res.send(response.acknowledged);
+                    client.close();
                 })
         })
         .catch(error => {
@@ -101,13 +102,47 @@ app.post('/species', (req, res) => {
         })
 })
 
-app.put('/species/:species/races', (req, res) => {
+app.post('/species/:species/races', (req, res) => {
     client.connect()
         .then(connection => {
             connection.db("mechanics").collection("species").updateOne({ name: req.params.species }, { $push: { races: req.body }})
                 .then(response => {
                     res.json(response);
+                    client.close();
                 })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/species/:species', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("species").deleteOne({name: req.params.species })
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/species/:species/race/:race', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("species").updateOne(
+                { name: req.params.species },
+                { $pull: { races : {  name: req.params.race  }}}
+            )
+            .then(response => {
+                res.send(response.modifiedCount);
+                client.close();
+            })
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
@@ -123,13 +158,16 @@ app.get('/classes', (req, res) => {
             connection.db("mechanics").collection("classes").find({}).toArray()
                 .then(classes => {
                     res.json(classes);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching classes:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -139,13 +177,16 @@ app.get('/classes/:name', (req, res) => {
             connection.db("mechanics").collection("classes").findOne({ name: `${req.params.name}` })
                 .then(classes => {
                     res.json(classes);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching class:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -155,13 +196,16 @@ app.get('/classes/:name/subclasses', (req, res) => {
             connection.db("mechanics").collection("classes").findOne({ name: `${req.params.name}` })
                 .then(classes => {
                     res.json(classes.subclasses);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching class:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -171,13 +215,79 @@ app.get('/classes/spellcaster/:isSpellcaster', (req, res) => {
             connection.db("mechanics").collection("classes").find({ "spellcasting.spellcaster": (req.params.isSpellcaster === "true") }).toArray()
                 .then(classes => {
                     res.json(classes);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching classes:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.post('/classes', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("classes").insertOne(req.body)
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.post('/classes/:class/subclasses', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("classes").updateOne({ name: req.params.class }, { $push: { "subclasses.selectable": req.body }})
+                .then(response => {
+                    res.json(response);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/classes/:class', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("classes").deleteOne({name: req.params.class })
+                .then(response => {
+                    res.send(response.modifiedCount);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/classes/:class/subclass/:subclass', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("classes").updateOne(
+                { name: req.params.class },
+                { $pull: { "subclasses.selectable": {  name: req.params.subclass  }}}
+            )
+            .then(response => {
+                res.send(response.modifiedCount);
+                client.close();
+            })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -189,13 +299,16 @@ app.get('/backgrounds', (req, res) => {
             connection.db("mechanics").collection("backgrounds").find({}).toArray()
                 .then(backgrounds => {
                     res.json(backgrounds);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching backgrounds:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -205,13 +318,46 @@ app.get('/backgrounds/:name', (req, res) => {
             connection.db("mechanics").collection("backgrounds").findOne({ name: req.params.name })
                 .then(backgrounds => {
                     res.json(backgrounds);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching background:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.post('/backgrounds', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("backgrounds").insertOne(req.body)
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/background/:background', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("backgrounds").deleteOne({name: req.params.background })
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -224,13 +370,16 @@ app.get('/spells', (req, res) => {
             connection.db("mechanics").collection("spells").find({}).toArray()
                 .then(spells => {
                     res.json(spells);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching spells:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -240,13 +389,65 @@ app.get('/spells/:name', (req, res) => {
             connection.db("mechanics").collection("spells").findOne({ name: req.params.name })
                 .then(spells => {
                     res.json(spells);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching spell:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.get('/spells/:element', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("spells").findOne({ type: req.params.element })
+                .then(spells => {
+                    res.json(spells);
+                    client.close();
+                })
+                .catch(error => {
+                    res.send(`Error fetching spell:' ${error}`);
+                    client.close();
+                });
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.post('/spells', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("spells").insertOne(req.body)
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/spells/:spell', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("spells").deleteOne({name: req.params.spell })
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -258,13 +459,16 @@ app.get('/feats', (req, res) => {
             connection.db("mechanics").collection("feats").find({}).toArray()
                 .then(feats => {
                     res.json(feats);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching feats:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
 
@@ -274,12 +478,45 @@ app.get('/feats/:name', (req, res) => {
             connection.db("mechanics").collection("feats").findOne({ name: req.params.name })
                 .then(feats => {
                     res.json(feats);
+                    client.close();
                 })
                 .catch(error => {
                     res.send(`Error fetching feat:' ${error}`);
+                    client.close();
                 });
         })
         .catch(error => {
             res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.post('/feats', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("feats").insertOne(req.body)
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
+        })
+})
+
+app.delete('/feats/:feat', (req, res) => {
+    client.connect()
+        .then(connection => {
+            connection.db("mechanics").collection("feats").deleteOne({name: req.params.feat })
+                .then(response => {
+                    res.send(response.acknowledged);
+                    client.close();
+                })
+        })
+        .catch(error => {
+            res.send(`Error connecting to MongoDB:' ${error}`);
+            client.close();
         })
 })
